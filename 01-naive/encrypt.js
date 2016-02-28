d66b1e71bacca706e6a5a59b23ec83ad728a4176
Paolo Montrasio <paolo@paolomontrasio.com>
2016-02-28 23:32:30 +0100

    Completed the naive encryption demo

--- 01-naive/encrypt.js
+++ 01-naive/encrypt.js
@@ -1,4 +1,31 @@
-fs = require('fs');
+const fs = require("fs");
+const crypto = require("crypto");
+
+function *main() {
+  const plaintext = yield readFile("plaintext.txt");
+  const publicKeyPem = yield readFile("../public_key.pem");
+  console.log("Encrypting:");
+  console.log("-----------------");
+  console.log(plaintext);
+  console.log("-----------------");
+  const ciphertext1 = crypto.publicEncrypt(publicKeyPem, new Buffer(plaintext));
+  const ciphertext2 = crypto.publicEncrypt(publicKeyPem, new Buffer(plaintext));
+  if (ciphertext1 == ciphertext2) {
+    // this is what you might expect
+    process.exit(0);
+  }
+  console.log("Surprise!");
+  console.log("Encryption is not deterministic. You got two different ciphertexts:");
+  console.log(ciphertext1);
+  console.log(ciphertext2);
+  const privateKeyPem = yield readFile("../private_key.pem");
+  console.log("\n\nBut they decrypt to the same plaintext:")
+  console.log("-----------------");
+  console.log(crypto.privateDecrypt(privateKeyPem, ciphertext1).toString("utf8"));
+  console.log("-----------------");
+  console.log(crypto.privateDecrypt(privateKeyPem, ciphertext2).toString("utf8"));
+  console.log("-----------------");
+}
 
 function readFile(file) {
   fs.readFile(file, "utf8", function (err, content) {
@@ -10,12 +37,5 @@ function readFile(file) {
   });
 }
 
-function *main() {
-  var plaintext = yield readFile("plaintext.txt");
-  console.log(plaintext);
-  var publicKey = yield readFile("../public_key.pem");
-  console.log(publicKey);
-}
-
-var encrypt = main();
+const encrypt = main();
 encrypt.next();
