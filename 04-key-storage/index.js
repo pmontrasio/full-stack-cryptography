d878ae65d215efd283d02a6ce7b8c368ddc3afc4
Giorgio Sidari <giorgio@sidari.it>
2016-04-17 13:38:11 +0200

    Example 04: saving to localStorage

---
+++ 04-key-storage/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const crypto = require("crypto");
+const constants = require("constants");
+const seedrandom = require("seedrandom");
+const express = require("express");
+const port = 4000;
+
+const app = express();
+app.use("/static", express.static("static"));
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+var data;
+const encrypt = prepare_data();
+encrypt.next();
+
+app.get("/", function (req, res) {
+  res.render("index", { data: data });
+});
+
+app.listen(port, function () {
+    console.log("Demo 04 app listening on http://localhost:" + port);
+});
+
+function *prepare_data() {
+  const plaintext = yield readFile("../plaintext.txt");
+  //console.log(plaintext);
+  const publicKeyPem = yield readFile("../public_key.pem");
+  //console.log(publicKeyPem);
+  const buffer = new Buffer(256);
+  const plaintextBuffer = new Buffer(plaintext, "utf8");
+  plaintextBuffer.copy(buffer);
+  buffer[plaintextBuffer.length] = 0;
+  const encryptionOptions = { key: publicKeyPem, padding: constants.RSA_NO_PADDING };
+  const rng = seedrandom(crypto.createHmac("sha256", plaintext));
+  for (var i = 256 - plaintextBuffer.length + 1; i < 256; i++) {
+    buffer[i] = Math.floor(rng() * 256);
+  }
+
+  // TODO encode in base64
+  data = crypto.publicEncrypt(encryptionOptions, buffer).toString("base64");
+  //console.log(data);
+}
+
+function readFile(file) {
+  fs.readFile(file, "utf8", function (err, content) {
+    if (err) {
+      console.log(err);
+    } else {
+      encrypt.next(content);
+    }
+  });
+}
---
+++ 04-key-storage/static/script.js
@@ -0,0 +1,49 @@
+const cyphertext = document.getElementById("base64").textContent;
+const openFormBtn = document.getElementById("open-form");
+const privateKeyForm = document.getElementById("private-key-form");
+const unknownKeyText = document.getElementById("unknown-key-text");
+const knownKeyText = document.getElementById("known-key-text");
+const privateKeyTextArea = document.getElementById("private-key");
+const decryptBtn = document.getElementById("decrypt");
+const plaintextDisplay = document.getElementById("plaintext");
+
+function addEventListener(el, eventName, handler) {
+  if (el.addEventListener) {
+    el.addEventListener(eventName, handler);
+  } else {
+    el.attachEvent("on" + eventName, function(){
+      handler.call(el);
+    });
+  }
+}
+
+addEventListener(openFormBtn, "click", function () {
+  if(localStorage.getItem('key'))
+    showStoredKey();
+  else
+    askKey();
+});
+
+addEventListener(decryptBtn, "click", function () {
+  if(!localStorage.getItem('key'))
+    localStorage.setItem('key', privateKeyTextArea.value);
+  const jse = new JSEncrypt();
+  jse.setKey(localStorage.getItem('key'));
+  const buffer = jse.decryptNoPadding(cyphertext);
+  const plaintext = buffer.substring(0, buffer.indexOf("\0"));
+  plaintextDisplay.textContent = plaintext;
+  plaintextDisplay.style.display = "inline";
+});
+
+function showStoredKey() {
+  privateKeyTextArea.textContent = localStorage.getItem('key');
+  knownKeyText.style.display = "inline";
+  unknownKeyText.style.display = "none";
+  privateKeyForm.style.display = "inline";
+}
+
+function askKey() {
+  knownKeyText.style.display = "none";
+  unknownKeyText.style.display = "inline";
+  privateKeyForm.style.display = "inline";
+}
